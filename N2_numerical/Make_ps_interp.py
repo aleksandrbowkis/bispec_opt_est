702dbd7731e9d1462154c547b00599a9a89c2092
Aleksandr Bowkis <aleksandr.bowkis@dialogic.co.uk>
2024-07-16 13:38:08 +0100

    Create ps

---
+++ N2_numerical/Make_ps_interp.py
@@ -0,0 +1,84 @@
+""" This code computes the power spectra used in the N2 low L approximations (and simulations etc) then saves them for loading into other code"""
+
+import numpy as np
+from numpy import random 
+from scipy.integrate import quad
+from scipy.interpolate import interp1d
+import sys, os
+sys.path.append('/home/amb257/software/cmplx_cmblensplus/wrap')
+sys.path.append('/home/amb257/software/cmplx_cmblensplus/utils')
+# from cmblensplus/wrap/
+import curvedsky as cs
+from scipy.integrate import quad
+
+
+################ Parameters ###############
+
+lmax = 2000
+Tcmb  = 2.726e6    # CMB temperature in microkelvin?
+rlmin, rlmax = 2, 2000 # CMB multipole range for reconstruction
+nside = 2048
+bstype = 'equi'
+nsims = 448 # Number of simulations to average over (in sets of 3) 
+ellmin = 2 
+ellmax = 2000 ##### check!!!! vs sims
+
+################ Power spectra ################
+
+ls, cl_unl, cl_len, cl_phi = np.loadtxt('/home/amb257/kappa_bispec/make_sims_parallel/camb_lencl_phi.txt')
+L = np.arange(rlmax+1)
+Lfac = (L*(L+1.) / 2 )**2
+lcl = cl_len[0:rlmax+1] / Tcmb**2
+ucl = cl_unl[0:rlmax+1] / Tcmb**2 #dimless unlensed T Cl
+cl_kappa = Lfac * cl_phi[0:rlmax+1]
+cl_phi = cl_phi[0:rlmax+1]
+
+#Make noise power spectra
+theta_fwhm = 1.4 #In arcminutes
+sigma_noise = 10 #in muK-arcmin
+arcmin2radfactor = np.pi / 60.0 / 180.0
+noise_cl = (sigma_noise*arcmin2radfactor/Tcmb)**2*np.exp(L*(L+1.)*(theta_fwhm*arcmin2radfactor)**2/np.log(2.)/8.)
+ocl = np.copy(lcl) + noise_cl
+
+# Interpolation functions for cl_kappa and ucl
+cl_phi_interp = interp1d(L, cl_phi, kind='cubic', bounds_error=False, fill_value="extrapolate")
+lcl_interp = interp1d(L, lcl, kind='cubic', bounds_error=False, fill_value="extrapolate")
+ctot_interp = interp1d(L, ocl, kind='cubic', bounds_error=False, fill_value="extrapolate")
+ctotprime = np.gradient(ocl, L)
+ctotprime_interp = interp1d(L, ctotprime, kind='cubic', bounds_error=False, fill_value="extrapolate")
+lclprime = np.gradient(lcl, L)
+lclprime_interp = interp1d(L, lclprime, kind='cubic', bounds_error=False, fill_value="extrapolate")
+lcldoubleprime = np.gradient(lclprime, L)
+lcldoubleprime_interp = interp1d(L, lcldoubleprime, kind='cubic', bounds_error=False, fill_value="extrapolate")
+
+
+# Now save everything
+
+# Create directory
+output_dir = "power_spec"
+os.makedirs(output_dir, exist_ok=True)
+
+# Save the power spectra
+
+np.save(os.path.join(output_dir, "L.npy"), L)
+np.save(os.path.join(output_dir, "cl_phi.npy"), cl_phi)
+np.save(os.path.join(output_dir, "cl_phi_interp.npy"), cl_phi_interp)
+np.save(os.path.join(output_dir, "lcl.npy"), lcl)
+np.save(os.path.join(output_dir, "lcl_interp.npy"), lcl_interp)
+np.save(os.path.join(output_dir, "ctot.npy"), ocl)
+np.save(os.path.join(output_dir, "ctot_interp.npy"), ctot_interp)
+np.save(os.path.join(output_dir, "ctotprime_interp.npy"), ctotprime_interp)
+np.save(os.path.join(output_dir, "lclprime_interp.npy"), lclprime_interp)
+np.save(os.path.join(output_dir, "lcldoubleprime_interp.npy"), lcldoubleprime_interp)
+
+
+
+
+np.save(os.path.join(output_dir, "lcl.npy"), lcl)
+np.save(os.path.join(output_dir, "ucl.npy"), ucl)
+
+np.save(os.path.join(output_dir, "noise_cl.npy"), noise_cl)
+np.save(os.path.join(output_dir, "ocl.npy"), ocl)
+np.save(os.path.join(output_dir, "ctotprime.npy"), ctotprime)
+np.save(os.path.join(output_dir, "lclprime.npy"), lclprime)
+np.save(os.path.join(output_dir, "lcldoubleprime.npy"), lcldoubleprime)
