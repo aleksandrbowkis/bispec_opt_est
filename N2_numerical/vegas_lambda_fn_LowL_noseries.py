e153caf4eabc1ebda75949b7c0e743382f2e333f
Aleksandr Bowkis <aleksandr.bowkis@dialogic.co.uk>
2024-09-30 13:37:53 +0100

    renaming, small edits

--- N2_numerical/vegas_lambda_fn_LowL_noseries.py
+++ N2_numerical/vegas_lambda_fn_LowL_noseries.py
@@ -1,9 +1,6 @@
-""" 
-    Program to compute the low L approximation to the N2 bias term in reconstructed CMB lensing bispectrum.
-"""
-
 import numpy as np
-from scipy.integrate import dblquad
+import vegas 
+from scipy.integrate import quad
 from scipy.interpolate import interp1d
 import sys, os
 sys.path.append('/home/amb257/software/cmplx_cmblensplus/wrap')
@@ -11,16 +8,15 @@ sys.path.append('/home/amb257/software/cmplx_cmblensplus/utils')
 # from cmblensplus/wrap/
 import curvedsky as cs
 
-################## Parameters ###############
 
+# Parameters
 lmax = 2000
 Tcmb  = 2.726e6    # CMB temperature in microkelvin?
 bstype = 'equi'
 ellmin, ellmax = 2, 2000
-rlmin, rlmax = 2, 2000
+rlmin, rlmax = 2, 2000 # CMB multipole range for reconstruction
 
-################## Load and interpolate power spectra ############
-
+# Define the directory where power spec are stored
 input_dir = "power_spec"
 
 # Load the power spec
@@ -34,7 +30,7 @@ cl_phi_interp = interp1d(L, cl_phi, kind='cubic', bounds_error=False, fill_value
 lcl_interp = interp1d(L, lcl, kind='cubic', bounds_error=False, fill_value="extrapolate")
 ctot_interp = interp1d(L, ctot, kind='cubic', bounds_error=False, fill_value="extrapolate")
 
-###################### Functions ################################
+################# Functions ##################
 
 def dotprod(l1,l2):
     #Computes the dot product of two (multipole) vectors.
@@ -62,7 +58,6 @@ def bigF(l1, l2, l1size, l2size, CTlens, Ctotal):
     return bigF
 
 def make_equilateral_L(sizeL):
-
     # Makes a set of 3 L's satisfying the triangle condition w/ L1 along the x axis and each L same length.
     L1, L2, L3 = np.zeros(2), np.zeros(2), np.zeros(2)
     L1[0] = sizeL
@@ -75,11 +70,20 @@ def make_equilateral_L(sizeL):
     
     return L1, L2, L3
 
-##################### Integrand ################################
+def make_fold_L(sizeL):
+    # Makes a set of 3 L's satisfying the condition L1 = 2L2 = 2L3 satisfying the triangle condition.
+    L1, L2, L3 = np.zeros(2), np.zeros(2), np.zeros(2)
+    L1[0] = sizeL
 
-def integrand_N2(ellx, elly, L1, L2, L3, lcl_interp, ctot_interp,cl_phi_interp):
+    L2[0] = -sizeL / 2
+    L3[0] = -sizeL / 2
+
+    return L1, L2, L3
+
+################ Integrand function (low L no series expansion) ############
+
+def integrand_N2(ell, L1, L2, L3, cl_phi_interp, lcl_interp, ocl_interp, ellmin, ellmax):
     
-    ell = np.array([ellx, elly])
     sizeell = vect_modulus(ell)
     ellminusL1 = ell - L1
     sizeellminusL1 = vect_modulus(ellminusL1)
@@ -89,39 +93,57 @@ def integrand_N2(ellx, elly, L1, L2, L3, lcl_interp, ctot_interp,cl_phi_interp):
     sizeL3 = vect_modulus(L3)
 
     if sizeell <= ellmax and sizeell >= ellmin and  sizeellminusL1 >= ellmin and sizeellminusL1 <= ellmax :
-        Fint1int = bigF(ell, L1-ell, sizeell, sizeellminusL1, lcl_interp, ctot_interp)
+        Fint1int = bigF(ell, L1-ell, sizeell, sizeellminusL1, lcl_interp, ocl_interp)
         N2_A = 2/((2*np.pi)**2)*Fint1int * cl_phi_interp(sizeL2) * cl_phi_interp(sizeL3) * lcl_interp(sizeell) * dotprod(ell, L2) * dotprod(ell, L3)   
     else:
         N2_A = 0
 
     if sizeell <= ellmax  and sizeell >= ellmin and  sizeellminusL1 >= ellmin and sizeellplusL3 <= ellmax and sizeellplusL3 >= ellmin and sizeellminusL1  <= ellmax:
-        Fint1int = bigF(ell, L1-ell, sizeell, sizeellminusL1, lcl_interp, ctot_interp)
+        Fint1int = bigF(ell, L1-ell, sizeell, sizeellminusL1, lcl_interp, ocl_interp)
         N2_B = - 2/((2*np.pi)**2)*Fint1int * cl_phi_interp(sizeL2) * cl_phi_interp(sizeL3) * lcl_interp(sizeellplusL3) * dotprod(ellplusL3, L2) * dotprod(ellplusL3, L3)   
     else:
         N2_B = 0
 
     return N2_A + N2_B
 
-#################### Lets get integratingggggg woooo ###########
 
-# Calculate the normalisation. Outside loop as unnecessary to repeatedly calculate.
+################ Main #############
+
+integration_limits = [[ellmin, ellmax], [ellmin, ellmax]] 
+lensingLarray = np.arange(1,1000,20)
+output_vegas = []
+
+# Now calculate the normalisation. Outside loop as unnecessary to repeatedly calculate.
 phi_norm, phi_curl_norm = {}, {}
 phi_norm['TT'], phi_curl_norm['TT'] = cs.norm_quad.qtt('lens',lmax,rlmin,rlmax,lcl,ctot,lfac='')
 
-# What L's (lensing L) are we calculating the integral for? 
-lensingLarray = np.arange(2,1000,20)
-output = []
-
 for lensingL in lensingLarray:
     L1, L2, L3 = make_equilateral_L(lensingL)
-    result, error = dblquad(lambda x, y: integrand_N2(x, y, L1, L2, L3, lcl_interp, ctot_interp,cl_phi_interp), ellmin, ellmax, lambda x: ellmin, lambda x: ellmax, epsrel=1e-5)
-    result *= phi_norm['TT'][int(lensingL)]
+    integrand_function = lambda ell: integrand_N2(ell, L1, L2, L3, cl_phi_interp, lcl_interp, ctot_interp, ellmin, ellmax) #second ps should be ctot_interp but changed to noiseless to cf giorgio's results 6/9/24
+    integrator = vegas.Integrator(integration_limits)
+    result = integrator(integrand_function, nitn=15, neval=1000)
 
-    output.append(result)
+    # Now output results
+    from gvar import mean
+    result_mean = mean(result)
 
-print(output)
+    # Now calculate the normalisation for this lensingL. then normalise result. Recall we're using phi everywhere as the low L N2 result was computed for phi.
+    # Also note we will want to plot N2 for kappa though for comparison.
+    norm_factor_phi = phi_norm['TT'][int(lensingL)]
+    result_mean *= norm_factor_phi
 
-output_dir = "scipy_results"
+    output_vegas.append(result_mean)
+
+# Change outputs to numpy arrays Factor of 3 for all equi.
+output_vegas = 3*np.array(output_vegas)
+
+#Convert to kappa
+#output_vegas *= (0.5*(lensingLarray*(lensingLarray+1)))^3
+
+# Save results
+# Create directory
+output_dir = "vegas_results"
 os.makedirs(output_dir, exist_ok=True)
-np.save(os.path.join(output_dir, "L.npy"), lensingLarray)
-np.save(os.path.join(output_dir, "scipy_lowL_noseries.npy"), output)
+np.save(os.path.join(output_dir, "lambda_L.npy"), lensingLarray)
+np.save(os.path.join(output_dir, "lambda_VEGAS_lowL.npy"), output_vegas)
+
