b9c0159f09622223cef9f483bb5e70e12a3fc730
Aleksandr Bowkis <aleksandr.bowkis@dialogic.co.uk>
2024-10-16 22:36:15 +0100

    Fixed integration  range

--- N0_numerical/flat_sky_normalisation.py
+++ N0_numerical/flat_sky_normalisation.py
@@ -60,7 +60,7 @@ def bigF(l1, l2, l1size, l2size, CTlens, Ctotal):
     bigF = response_func(CTlens, l1, l2, l1size, l2size) / ( 2 * Ctotal(l1size) * Ctotal(l2size))
     return bigF
 
-@vg.batchintegrand
+#@vg.batchintegrand
 def integrand(x, L, gcl_interp, ctot_interp):
     x = np.atleast_2d(x) # Make x into a 2d array as expected in code below
     # x[:, 0] is the x-component of ell, x[:, 1] is the y-component of ell
@@ -69,7 +69,7 @@ def integrand(x, L, gcl_interp, ctot_interp):
     sizeell = np.sqrt(np.sum(ell**2, axis=1)) # Allows modulus for each point in the 2D array of points to be computed (N rows of points with 2 columns, for x and y coordinates of a single point)
     Lminusell = L[np.newaxis, :] - ell # Broadcasting allows every point that vegas sends in batch mode from the same L1
     sizeLminusell = np.sqrt(np.sum(Lminusell**2, axis=1))
-    print('lminus',Lminusell)
+    #print('lminus',Lminusell)
     # mask out disallowed regions
     mask = (ellmin <= sizeell) & (sizeell <= ellmax) & \
            (ellmin <= sizeLminusell) & (sizeLminusell <= ellmax)
@@ -85,10 +85,10 @@ def integrand(x, L, gcl_interp, ctot_interp):
     return integrand
 
 def compute_normalisation(L, gcl_interp, ctot_interp, ellmin, ellmax):
-    integration_limits = [[ellmin, ellmax], [ellmin, ellmax]]
+    integration_limits = [[-ellmax, ellmax], [-ellmax, ellmax]]
     integrator = vg.Integrator(integration_limits)
     
-    result = integrator(lambda x: integrand(x, L, gcl_interp, ctot_interp), nitn=1, neval=100)
+    result = integrator(lambda x: integrand(x, L, gcl_interp, ctot_interp), nitn=10, neval=10000)
 
     from gvar import mean
     result_mean = mean(result)
@@ -102,9 +102,16 @@ def compute_normalisation(L, gcl_interp, ctot_interp, ellmin, ellmax):
 
 ##### Main #####
 def main():
-    L_output = np.arange(0, 5, 5)
+    L_output = np.arange(0, 2001, 1)
     output_dir = "normalisation"
     os.makedirs(output_dir, exist_ok=True)
+    # results = []
+    # for L in L_output:
+    #     intermediate_result = compute_normalisation(L, gcl_interp, ctot_interp, ellmin, ellmax)
+    #     results.append(intermediate_result)
+        
+
+    # print(results)
 
     # Use multiprocessing to parallelise over the lensing L's (calculate integral for each lensing L) 
     with mp.Pool(processes=mp.cpu_count()) as pool:
@@ -113,7 +120,7 @@ def main():
     # Convert the results to a numpy array and save
     output = np.array(results)
     np.save(os.path.join(output_dir, "L_norm.npy"), L_output)
-    np.save(os.path.join(output_dir, "lensedcl_flat_sky_norm.npy"), output)
+    np.save(os.path.join(output_dir, "flat_sky_norm.npy"), output)
 
 if __name__ == '__main__':
     main()
