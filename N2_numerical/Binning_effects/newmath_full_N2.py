da2e6a31624c0bb5bf2b30e341a3d8f06f87a7c1
Aleksandr Bowkis <aleksandr.bowkis@dialogic.co.uk>
2025-01-23 11:08:51 +0000

    Update binning calculation to compute binning effect on folded configurations

--- N2_numerical/Binning_effects/newmath_full_N2.py
+++ N2_numerical/Binning_effects/newmath_full_N2.py
@@ -4,64 +4,29 @@
 """
 
 import numpy as np
-from numpy import random 
 from scipy.integrate import quad
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d # Still needed for norm_phi_interp
 import sys
 sys.path.append('/home/amb257/software/cmplx_cmblensplus/wrap')
 sys.path.append('/home/amb257/software/cmplx_cmblensplus/utils')
-# from cmblensplus/wrap/
-import curvedsky as cs
+sys.path.append('/home/amb257/kappa_bispec/bispec_opt_est/Configuration/')
+import curvedsky as cs # from cmblensplus/wrap/
+from config import CMBConfig # Import CMBConfig class from config.py
 
-################ Parameters ###############
-
-Tcmb  = 2.726e6    # CMB temperature in microkelvin?
-rlmin, rlmax = 2, 3000 # CMB multipole range for reconstruction
-nside = 2048
-bstype = 'fold'
-nsims = 448 # Number of simulations to average over (in sets of 3) 
-ellmin = 2 
-ellmax = 3000 ##### 30/09/24 CHANGED TO 3000 CF SIMULATED RESULTS
-
-################ Power spectra ################
-
-ls, cl_unl, cl_len, cl_phi = np.loadtxt('/home/amb257/kappa_bispec/make_sims_parallel/camb_lencl_phi.txt')
-L = np.arange(rlmax+1)
-Lfac = (L*(L+1.) / 2 )**2
-lcl = cl_len[0:rlmax+1] / Tcmb**2
-ucl = cl_unl[0:rlmax+1] / Tcmb**2 #dimless unlensed T Cl
-cl_kappa = Lfac * cl_phi[0:rlmax+1]
-cl_phi = cl_phi[0:rlmax+1]
-
-#Make noise power spectra
-theta_fwhm = 1.4 #In arcminutes
-sigma_noise = 10 #in muK-arcmin
-arcmin2radfactor = np.pi / 60.0 / 180.0
-noise_cl = (sigma_noise*arcmin2radfactor/Tcmb)**2*np.exp(L*(L+1.)*(theta_fwhm*arcmin2radfactor)**2/np.log(2.)/8.)
-ocl = np.copy(lcl) + noise_cl #INC NOISE CF SIMULATED RESULTS NB EQUI NOISELESS ATM TO CF GIORGIO
-
-# Interpolation functions for cl_kappa and ucl
-cl_phi_interp = interp1d(L, cl_phi, kind='cubic', bounds_error=False, fill_value="extrapolate")
-lcl_interp = interp1d(L, lcl, kind='cubic', bounds_error=False, fill_value="extrapolate")
-ctot_interp = interp1d(L, ocl, kind='cubic', bounds_error=False, fill_value="extrapolate")
-ctotprime = np.gradient(ocl, L)
-ctotprime_interp = interp1d(L, ctotprime, kind='cubic', bounds_error=False, fill_value="extrapolate")
-lclprime = np.gradient(lcl, L)
-lclprime_interp = interp1d(L, lclprime, kind='cubic', bounds_error=False, fill_value="extrapolate")
-lcldoubleprime = np.gradient(lclprime, L)
-lcldoubleprime_interp = interp1d(L, lcldoubleprime, kind='cubic', bounds_error=False, fill_value="extrapolate")
+# Import config class
+config = CMBConfig()
 
 #### Integration functions ####
 
-def calculate_n2_integrand(l, L1, L2, L3, x1, x2, x3, ctot_interp, lcl_interp, 
-                     ctotprime_interp, lclprime_interp, lcldoubleprime_interp):
+def calculate_n2_integrand(l, L1, L2, L3, x1, x2, x3, cphi_interp, ctot_interp, lcl_interp, 
+                     ctotprime_interp, lclprime_interp, lcldoubleprime_interp, norm_phi_interp):
     """
-    Calculate the N2 bias to the reconstructed lensing bispectrum for a specific
-    configuration.
+    Calculate the N2 bias to the reconstructed lensing bispectrum for any configuration
+    Note this returns only one permutation the remaining two can be found by: L1<->L2 and L1<->L3
     
     Args:
         l, L1, L2, L3: Multipole values
-        x1, x2, x3: Angular values
+        x1, x2, x3: Angle of each triangle side
         ctot_interp: Interpolated total power spectrum (signal + noise)
         lcl_interp: Interpolated lensed CMB power spectrum
         ctotprime_interp: Interpolated derivative of total power spectrum
@@ -69,7 +34,7 @@ def calculate_n2_integrand(l, L1, L2, L3, x1, x2, x3, ctot_interp, lcl_interp,
         lcldoubleprime_interp: Interpolated second derivative of lensed power spectrum
     
     Returns:
-        float: N2_integrand for folded configuration
+        float: N2 for general configuration for reconstructed lensing bispectrum for KAPPA!
     """
     # Get interpolated values
     Ctot = ctot_interp(l)
@@ -77,6 +42,11 @@ def calculate_n2_integrand(l, L1, L2, L3, x1, x2, x3, ctot_interp, lcl_interp,
     Ctot_prime = ctotprime_interp(l)
     Ctt_prime = lclprime_interp(l)
     Ctt_doubleprime = lcldoubleprime_interp(l)
+
+    # Define all normalisations
+    A_L1 = norm_phi_interp(L1)
+    A_L2 = norm_phi_interp(L2)
+    A_L3 = norm_phi_interp(L3)
     
     # Precompute some common cosine terms
     cos_x1_m_x2 = np.cos(x1 - x2)
@@ -86,7 +56,7 @@ def calculate_n2_integrand(l, L1, L2, L3, x1, x2, x3, ctot_interp, lcl_interp,
     cos_2x1_p_x2_m_3x3 = np.cos(2*x1 + x2 - 3*x3)
     cos_2x1_m_x2_m_x3 = np.cos(2*x1 - x2 - x3)
     
-    # First term in parentheses
+    # First term 
     term1 = -2 * l * L1 * Ctot_prime * (
         8 * (3*cos_x1_m_x2 + cos_x1_p_x2_m_2x3) * Ctt**2 +
         2 * l * (13*cos_x1_m_x2 + 5*cos_x1_p_x2_m_2x3) * Ctt * Ctt_prime +
@@ -94,7 +64,7 @@ def calculate_n2_integrand(l, L1, L2, L3, x1, x2, x3, ctot_interp, lcl_interp,
                 3*cos_x1_p_x2_m_2x3) * Ctt_prime**2
     )
     
-    # Second term in parentheses
+    # Second term 
     term2 = -Ctot * (
         64 * L3 * cos_x2_m_x3 * Ctt**2 -
         2 * l * Ctt * (
@@ -115,55 +85,63 @@ def calculate_n2_integrand(l, L1, L2, L3, x1, x2, x3, ctot_interp, lcl_interp,
     
     # Combine terms with prefactor
     prefactor = -(1 / (128 * np.pi * Ctot**3))
+    kappa_factor = L1*(L1+1) * L2*(L2+1) * L3*(L3+1) / 8
     
-    result = prefactor * l * L1**2 * L2 * L3**2 * (term1 + term2)
+    result = kappa_factor * prefactor * l * L1**2 * L2 * L3**2 * A_L1 * cphi_interp(L2) * cphi_interp(L3) * (term1 + term2)
     
     return result
 
-######## Main function ########
-
-##################### Test for fold ####################
-
-
+######## Main function where test this in folded and equilateral limit ########
 # Define L values to compute integral for
 lensingLarray = np.arange(2, 1000, 10)
-# Output array
-output_quad = []
+# Output arrays
+output_fd = []
+output_eq = []
 # Normalisation
 phi_norm, phi_curl_norm = {}, {}
-phi_norm['TT'], phi_curl_norm['TT'] = cs.norm_quad.qtt('lens',rlmax,rlmin,rlmax,lcl,ocl,lfac='')
-norm_factor_phi = interp1d(L, phi_norm['TT'], kind='cubic', bounds_error=False, fill_value="extrapolate")
+phi_norm['TT'], phi_curl_norm['TT'] = cs.norm_quad.qtt('lens',config.rlmax,config.rlmin,config.rlmax,config.lcl,config.ocl,lfac='')
+norm_factor_phi = interp1d(config.L, phi_norm['TT'], kind='cubic', bounds_error=False, fill_value="extrapolate")
 
 #Define triangle shape for equilateral:
-# x1 = 0
-# x2 = 2*np.pi/3
-# x3 = 4*np.pi/3
+x1_eq = 0
+x2_eq = 2*np.pi/3
+x3_eq = 4*np.pi/3
 
 
 # Define triangle shape for folded:
-x1 = 0
-x2 = np.pi
-x3 = np.pi
+x1_fd = 0
+x2_fd = np.pi
+x3_fd = np.pi
 
 for lensingL in lensingLarray:
     #Define triangle side lengths (equilateral):
-    # L1 = lensingL
-    # L2 = lensingL
-    # L3 = lensingL
+    L1_eq = lensingL
+    L2_eq = lensingL
+    L3_eq = lensingL
 
     #Define triangle side lengths (folded):
-    L1 = lensingL
-    L2 = lensingL/2
-    L3 = lensingL/2
+    L1_fd = lensingL
+    L2_fd = lensingL/2
+    L3_fd = lensingL/2
 
     # Using scipy's quad for numerical integration
-    
-    result, error = quad(lambda l: calculate_n2_integrand(l, L1, L2, L3, x1, x2, x3, ctot_interp, lcl_interp, ctotprime_interp, lclprime_interp, lcldoubleprime_interp), ellmin, ellmax)
+    perm1_fd, error = quad(lambda l: calculate_n2_integrand(l, L1_fd, L2_fd, L3_fd, x1_fd, x2_fd, x3_fd, config.cl_phi_interp, config.ctot_interp, config.lcl_interp, config.ctotprime_interp, config.lclprime_interp, config.lcldoubleprime_interp, norm_factor_phi), config.ellmin, config.ellmax)
+    perm2_fd, error = quad(lambda l: calculate_n2_integrand(l, L2_fd, L1_fd, L3_fd, x2_fd, x1_fd, x3_fd, config.cl_phi_interp, config.ctot_interp, config.lcl_interp, config.ctotprime_interp, config.lclprime_interp, config.lcldoubleprime_interp, norm_factor_phi), config.ellmin, config.ellmax)
+    perm3_fd, error = quad(lambda l: calculate_n2_integrand(l, L3_fd, L2_fd, L1_fd, x3_fd, x2_fd, x1_fd, config.cl_phi_interp, config.ctot_interp, config.lcl_interp, config.ctotprime_interp, config.lclprime_interp, config.lcldoubleprime_interp, norm_factor_phi), config.ellmin, config.ellmax)
 
-    output_quad.append(result)
+    perm1_eq, error = quad(lambda l: calculate_n2_integrand(l, L1_eq, L2_eq, L3_eq, x1_eq, x2_eq, x3_eq, config.cl_phi_interp, config.ctot_interp, config.lcl_interp, config.ctotprime_interp, config.lclprime_interp, config.lcldoubleprime_interp, norm_factor_phi), config.ellmin, config.ellmax)
+    perm2_eq, error = quad(lambda l: calculate_n2_integrand(l, L2_eq, L1_eq, L3_eq, x2_eq, x1_eq, x3_eq, config.cl_phi_interp, config.ctot_interp, config.lcl_interp, config.ctotprime_interp, config.lclprime_interp, config.lcldoubleprime_interp, norm_factor_phi), config.ellmin, config.ellmax)
+    perm3_eq, error = quad(lambda l: calculate_n2_integrand(l, L3_eq, L2_eq, L1_eq, x3_eq, x2_eq, x1_eq, config.cl_phi_interp, config.ctot_interp, config.lcl_interp, config.ctotprime_interp, config.lclprime_interp, config.lcldoubleprime_interp, norm_factor_phi), config.ellmin, config.ellmax)
+    
+    integral_fd = perm1_fd + perm2_fd + perm3_fd
+    integral_eq = perm1_eq + perm2_eq + perm3_eq
+    output_fd.append(integral_fd)
+    output_eq.append(integral_eq)
 
 # Change outputs to numpy arrays
-output_quad = np.array(output_quad)
+output_fd = np.array(output_fd)
+output_eq = np.array(output_eq)
 
 # Save result
-np.savetxt('test_outputs/N2_from_full_int', (lensingLarray, output_quad))
+np.savetxt('test_outputs/foldN2_from_full_int.txt', (lensingLarray, output_fd))
+np.savetxt('test_outputs/equilN2_from_full_int.txt', (lensingLarray, output_eq))
